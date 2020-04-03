54cf6e8201858cd6513374bb0b6c4112997afa3b
Mike England <mike.england@webdevstudios.com>
2020-04-02 23:21:27 -0400

    Setup singular page app from week-1
    - Add components from week-1 project
    - Remove logo import since this file is no longer being included
    - Delete logo file
    
    See week-2 project guidelines

--- week-2/project/one-page-app/src/App.js
+++ week-2/project/one-page-app/src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Static Values
+const siteTitle = "Week 2 Project";
+
+// Site Header
+const Header = () => (
+  <header>
+    <h1>
+      <a href="http://react.com">{siteTitle}</a>
+    </h1>
+  </header>
+);
+
+const requirementContent = [
+  "Static Header",
+  "Main Content Area",
+  "Static Footer"
+];
+
+const RequirementsList = () => {
+  const listElements = requirementContent.map((x, id) => <li key={id}>{x}</li>);
+
+  return <ul>{listElements}</ul>;
+};
+
+// Site Content Area
+const Main = () => (
+  <main>
+    <p>This is my content area.</p>
+    <RequirementsList />
+  </main>
+);
+
+// Site Footer Area
+const Footer = () => (
+  <footer>
+    <i>simple one page website with a static header, footer and main content area</i>
+    <p>
+      <strong>{siteTitle} | 3-3-2020</strong>
+    </p>
+  </footer>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Fragment>
+        <Header />
+        <Main />
+        <Footer />
+      </React.Fragment>
     </div>
   );
 }
