02bf89ed2c375193fa44e0d770b1f467e19f039b
Mike England <mike.england@webdevstudios.com>
2020-04-06 22:14:37 -0400

    Setup Practice2 component
    - Add object values to post.
    - Add attributes into Post component from post object.
    - Pass in destructured id and title props into Post component.
    - Update Post component render markup.
    
    See Chapter 11 exercise 2

--- week-3/practice/starter/src/Practice2.js
+++ week-3/practice/starter/src/Practice2.js
@@ -4,13 +4,21 @@ const Practice2 = () => {
   /*
     1. Create post object with an id and title
   */
-  const post = {};
+  const post = {
+    id: 1,
+    title: 'Black Summer',
+  };
+
   return (
     <div className="practice">
-      {/* 
+      {/*
         2. Call the Post component below
         3. Pass in the post object as a prop
       */}
+      <Post
+        id={ post.id }
+        title={ post.title }
+      />
     </div>
   );
 };
@@ -19,8 +27,16 @@ const Practice2 = () => {
   4. Make the Post component accept props
   5. Have Post component rendr the post title and ID to the page
 */
-const Post = () => {
-  return <p>Render post title and id here</p>;
+const Post = ({ id, title }) => {
+  console.log( id, title );
+
+  return (
+    <>
+      <h1>{ title }</h1>
+      <p>post id:{ id }</p>
+    </>
+   );
+  // return <p>Render post title and id here</p>;
 };
 
 export default Practice2;
