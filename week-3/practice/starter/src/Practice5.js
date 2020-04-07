c09cfa2e5e353f678d6f6e42758733eef5e8830e
Mike England <mike.england@webdevstudios.com>
2020-04-06 23:09:31 -0400

    Setup exercise 5 components
    - Passed user props in using the spread operator.
    - Destructured all used props in `<User />` component.
    - Add passed in prop values into components.
    - Destructured `<FullName />` component props.
    - Create simple `<Username />` component with destructured props.
    - Update `<Social />` component with destructured props and updated render
    method markup.
    
    See Chapter 11 exercise 5

--- week-3/practice/starter/src/Practice5.js
+++ week-3/practice/starter/src/Practice5.js
@@ -12,26 +12,44 @@ const Practice5 = () => {
   };
   return (
     <div className="practice">
-      {/*         
+      {/*
         1. Spread the "user" object into User so each "user" property becomes it's own prop
       */}
-      <User />
+      <User
+        { ...user }
+      />
     </div>
   );
 };
 
 /*
-  2. Destructure all of the props you will need 
+  2. Destructure all of the props you will need
   3. Pass the firstName and lastName to <FullName />
   4. Pass the username to <Username />
   5. Pass the url and twitter to <Social />
 */
-const User = () => {
+const User = ( props ) => {
+  const {
+    firstName,
+    lastName,
+    username,
+    twitter,
+    url,
+  } = props;
+
   return (
     <div className="user">
-      <FullName />
-      <Username />
-      <Social />
+      <FullName
+        firstName={ firstName }
+        lastName={ lastName }
+      />
+      <Username
+        username={ username }
+      />
+      <Social
+        websiteURL={ url }
+        twitterURL={ `https://twitter.com/${ twitter }` }
+      />
     </div>
   );
 };
@@ -39,7 +57,7 @@ const User = () => {
 /*
   6. Destructure the props needed
 */
-const FullName = props => (
+const FullName = ( { firstName, lastName } ) => (
   <h1>
     {firstName} {lastName}
   </h1>
@@ -48,19 +66,27 @@ const FullName = props => (
 /*
   7. Create a <Username /> component that displays the username
 */
+const Username = ( { username } ) => (
+  <p>Username: { username }</p>
+);
 
 /*
   8. Destructure the props you will need
   9. Make the Website and Twitter links work based on props
 */
 const Social = props => {
+  const {
+    websiteURL,
+    twitterURL,
+  } = props;
+
   return (
     <ul className="social">
       <li>
-        <a>Website</a>
+        <a href={ websiteURL }>Website</a>
       </li>
       <li>
-        <a>Twitter</a>
+        <a href={ twitterURL }>Twitter</a>
       </li>
     </ul>
   );
