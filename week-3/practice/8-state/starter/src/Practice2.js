00eaf271177b533c6d70774e9f22edb64e90113e
Mike England <mike.england@webdevstudios.com>
2020-04-08 22:07:18 -0400

    Complete exercise 2
    - Update main index
    - Setup handleUsername event hanlder function
    - Update input markup
    
    See Chapter 13 exercise 2

--- week-3/practice/8-state/starter/src/Practice2.js
+++ week-3/practice/8-state/starter/src/Practice2.js
@@ -5,23 +5,31 @@ class Practice2 extends React.Component {
     username: "yourusername"
   };
 
-  /* 
+  /*
     1. Create an arrow function named handleUsername
     2. Accept `e` as a parameter for the event object
-    3. Update the value of username in state with the value 
+    3. Update the value of username in state with the value
         from the form (e.target.value)
   */
+  handleUsername = e => {
+    console.log( e );
+    return (
+      this.setState(
+        { username: e.target.value }
+      )
+    );
+  };
 
   render() {
     return (
       <>
         <p>Username: {this.state.username}</p>
         <p>
-          {/* 
+          {/*
             4. onChange should call this.handleUsername
             5. Set placeholder to the username in state
           */}
-          <input onChange="" type="text" placeholder="" />
+          <input onChange={ this.handleUsername } type="text" placeholder={ this.state.username } />
         </p>
       </>
     );
