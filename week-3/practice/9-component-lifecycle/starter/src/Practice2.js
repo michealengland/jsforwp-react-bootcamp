777d355a0def45903da1aad68cafa09e7ff63a8a
Mike England <mike.england@webdevstudios.com>
2020-04-08 23:19:51 -0400

    Complete exercise 2
    - Added componentDidMount function
    - Defined fetch based on book example
    
    See Chapter 15 exercise 2

--- week-3/practice/9-component-lifecycle/starter/src/Practice2.js
+++ week-3/practice/9-component-lifecycle/starter/src/Practice2.js
@@ -11,13 +11,22 @@ class Practice2 extends React.Component {
       }
     ]
   };
-  /* 
+  /*
     1. Add the method componentDidMount()
     2. Call fetch("https://dev-react-explained-api.pantheonsite.io/wp-json/wp/v2/posts")
     3. Then call .json() on the response
     4. Take that and set it as the value of posts in state
     5  Add a catch to log out any errors
   */
+  componentDidMount() {
+    fetch("https://dev-react-explained-api.pantheonsite.io/wp-json/wp/v2/posts")
+    .then( response => response.json() )
+    .then( posts => {
+      this.setState({ posts: posts })
+    } )
+    .catch( error => console.error(error) );
+  }
+
   render() {
     return (
       <header>
