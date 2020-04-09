c5967df0430d5abfa6503c79fe9289e83408821d
Mike England <mike.england@webdevstudios.com>
2020-04-08 22:48:16 -0400

    Complete exercise 4
    - Setup Header component with Text props
    - Setup Button component with text and onClick props
    - Set state on count to zero
    - Create increment, decremenent, and reset function that utilize setState
    to update values
    - Add components to render method
    
    See Chapter 13 exercise 4

--- week-3/practice/8-state/starter/src/Practice4.js
+++ week-3/practice/8-state/starter/src/Practice4.js
@@ -4,24 +4,51 @@ import React from "react";
   1. Create a functional component called Header
   2. Return an <h2> with the value of text from props.text
 */
+const Header = ( { text } ) => (
+  <h2>{ text }</h2>
+);
 
 /*
   3. Create a functional component called Button
-  4. Return an <input> with the value for onCLick set 
+  4. Return an <input> with the value for onCLick set
       to props.onClick and the value of the Button text
-      set to props.text      
+      set to props.text
 */
+const Button = ( { text, onClick } ) => (
+  <button
+    onClick={ onClick }
+  >
+    { text }
+  </button>
+);
 
 class Practice4 extends React.Component {
   /*
     5. Setup state with a count property set to 0
   */
 
+  // Traditional
+  // constructor( props ) {
+  //   super( props );
+
+  //   this.state = {
+  //     count: 0
+  //   }
+  // }
+
+  // Simplified
+  state = {
+    count: 0,
+  }
+
   /*
     6. Create an incremenent function that adds one to the value of count in state
     7. Create a decremenent function that subtracts one from the value of count in state
     8. Create a reset function that sets the value of count in state back to 0
   */
+  increment = () => ( this.setState({ count: this.state.count + 1 }) );
+  decrement = () => ( this.setState({ count: this.state.count - 1 }) );
+  reset     = () => ( this.setState({ count: 0 }) );
 
   render() {
     return (
@@ -32,6 +59,21 @@ class Practice4 extends React.Component {
           11. Call Button with onClick set to increment and text "+"
           12. Call Button with onClick set to reset and text "Reset"
         */}
+        <Header
+          text={ this.state.count }
+        />
+        <Button
+          onClick={ this.decrement }
+          text="-"
+        />
+        <Button
+          onClick={ this.increment }
+          text="+"
+        />
+        <Button
+          onClick={ this.reset }
+          text="reset"
+        />
       </>
     );
   }
