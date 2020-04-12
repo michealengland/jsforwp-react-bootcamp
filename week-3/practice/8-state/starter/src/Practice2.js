560894c2d5c5d4846fae884d22cb53fff136cada
Mike England <mike.england@webdevstudios.com>
2020-04-12 13:12:08 -0400

    Fix merges error
    Accidental conflicts that were merged

--- week-3/practice/8-state/starter/src/Practice2.js
+++ week-3/practice/8-state/starter/src/Practice2.js
@@ -5,14 +5,6 @@ class Practice2 extends React.Component {
     username: "yourusername"
   };
 
-<<<<<<< HEAD
-  /* 
-    1. Create an arrow function named handleUsername
-    2. Accept `e` as a parameter for the event object
-    3. Update the value of username in state with the value 
-        from the form (e.target.value)
-  */
-=======
   /*
     1. Create an arrow function named handleUsername
     2. Accept `e` as a parameter for the event object
@@ -27,26 +19,17 @@ class Practice2 extends React.Component {
       )
     );
   };
->>>>>>> week-3/practice
 
   render() {
     return (
       <>
         <p>Username: {this.state.username}</p>
         <p>
-<<<<<<< HEAD
-          {/* 
-            4. onChange should call this.handleUsername
-            5. Set placeholder to the username in state
-          */}
-          <input onChange="" type="text" placeholder="" />
-=======
           {/*
             4. onChange should call this.handleUsername
             5. Set placeholder to the username in state
           */}
           <input onChange={ this.handleUsername } type="text" placeholder={ this.state.username } />
->>>>>>> week-3/practice
         </p>
       </>
     );
