c7bfa0b1f2e2f2e24274f4e1f19181ed21de37f1
Mike England <mike.england@webdevstudios.com>
2020-04-19 11:31:24 -0400

    Complete hooks exercise 1
    - Import React & Use useState.
    - Setup useState destructure for count and setCount.
    - Create counter increment function.
    - Add count to H1
    - Setup button to use counter increment.

--- week-4/practice/11-hooks/starter/src/Practice1.js
+++ week-4/practice/11-hooks/starter/src/Practice1.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Practice1 = () => {
   /*
@@ -7,10 +7,16 @@ const Practice1 = () => {
    * 3. Display the count in an h1
    * 4. Add a button to increase the count
    */
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => setCount( count + 1 );
 
   return (
     <>
-      <h1>Counter</h1>
+      <h1>Counter: { count }</h1>
+      <button onClick={ increaseCount }>
+        Increment
+      </button>
     </>
   );
 };
