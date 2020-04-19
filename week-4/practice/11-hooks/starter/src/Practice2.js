5aac7b44b23719f31a63edd3f8c85e648d145feb
Mike England <mike.england@webdevstudios.com>
2020-04-19 11:43:16 -0400

    Complete Hooks exercise 2
    - Setup points state for homePoints and awayPoints.
    - Setup increment functions for points.
    - Setup reset count function that sets both points state to 0.
    - Add buttons and onClick functions in return.

--- week-4/practice/11-hooks/starter/src/Practice2.js
+++ week-4/practice/11-hooks/starter/src/Practice2.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Practice2 = () => {
   /*
@@ -7,6 +7,20 @@ const Practice2 = () => {
    * 3. Add a button for each team to add one point
    * 4. Add an onClick event to the Reset button that sets both points to zero
    */
+
+  // Setup state for both scores starting with 0.
+  const [homePoints, setHomePoints] = useState( 0 );
+  const [awayPoints, setAwayPoints] = useState( 0 );
+
+  // Increment each score by one.
+  const incrementHomePoints = () => setHomePoints( homePoints + 1 );
+  const incrementAwayPoints = () => setAwayPoints( awayPoints + 1 );
+
+  // Set both scores to zero.
+  const resetScore = () => (
+    setHomePoints( 0 ),
+    setAwayPoints( 0 )
+  );
 
   return (
     <>
@@ -18,11 +32,17 @@ const Practice2 = () => {
           justifyContent: `space-between`,
         }}
       >
-        <h2>Home: POINTS</h2>
-        <h2>Away: POINTS</h2>
+        <div>
+          <h2>Home: { homePoints }</h2>
+          <button onClick={ incrementHomePoints }>Increase Home Point</button>
+        </div>
+        <div>
+          <h2>Away: { awayPoints }</h2>
+          <button onClick={ incrementAwayPoints }>Increase Away Point</button>
+        </div>
       </div>
       <p>
-        <button>Reset Points</button>
+        <button onClick={ resetScore }>Reset Points</button>
       </p>
     </>
   );
