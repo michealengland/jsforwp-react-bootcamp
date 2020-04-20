8e84e4c56b988ca0f7f51c425353281e9847e5d0
Mike England <mike.england@webdevstudios.com>
2020-04-20 00:36:22 -0400

    Setup basic post feed
    - Add import for useState and useEffect
    - Create Posts component
    - Setup initial state for posts
    - Create fetchWordPressPost function
    - Setup fetchWordPressPost and console statement with useEffect
    - Create listPosts function to loop through posts objects
    - Conditionally render posts or loading message
    - Update app render

--- week-4/project/wordpress-post-reader/src/App.js
+++ week-4/project/wordpress-post-reader/src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const Posts = () => {
+
+  // Set initial state on posts.
+  const [posts, setPosts] = useState();
+
+  const fetchWordPressPost = () => {
+    fetch( `https://demo.wp-api.org/wp-json/wp/v2/posts` )
+      .then( response => response.json() )
+      .then( posts => setPosts( posts ) )
+      .catch( error => console.error( error ) );
+  };
+
+  // Log posts anytime posts state is updated.
+  useEffect(() => (
+    console.log( posts )
+  ), [posts]);
+
+  // Run fetch on page load only.
+  useEffect(() => {
+    fetchWordPressPost();
+  }, []);
+
+  const listPosts = () => (
+    Object.entries( posts ).map( ( post, id ) => {
+      const currentPost = post[1];
+      return (
+        <Post
+          author={ currentPost.author }
+          date={ currentPost.date }
+          excerpt={ currentPost.excerpt.rendered }
+          id={ currentPost.id }
+          key={ id }
+          link={ currentPost.link }
+          title={ currentPost.title.rendered }
+        />
+      );
+  } ) );
+
+  return (
+    <main>
+      { posts ? listPosts() : 'loading posts...' }
+    </main>
+  );
+}
+
+// Individual Post View.
+const Post = ( { author, date, excerpt, id, link, title } ) => (
+  <article id={ `post-id-${ id }` }>
+    <h2><a href={ link }>{ title }</a></h2>
+    <span>{ author }</span><br />
+    <span>{ date }</span>
+    <p>{ excerpt }</p>
+  </article>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Posts />
     </div>
   );
 }
